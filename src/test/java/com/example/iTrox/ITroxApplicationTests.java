2ee49d505543a321d2d11deb9e72b990e1ca4a8a
Aayush Raj <batmac@Aayushs-MacBook-Air.local>
2023-01-07 12:48:37 +0530

    first commit

---
+++ src/test/java/com/example/iTrox/ITroxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iTrox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITroxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
